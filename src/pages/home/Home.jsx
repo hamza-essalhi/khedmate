f6789a9756a0054aa820481d36e8ab90e6b90651
hamza-essalhi <hamzaessalhix@gmail.com>
2023-05-04 02:12:44 +0100

    Adding pagenation whit 10 jobs in page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,28 +2,49 @@ import { FaSearch } from "react-icons/fa";
 
 import Select from "../components/Select";
 import { useEffect, useState } from "react";
-import usersData from '../../users.json';
+import usersData from "../../users.json";
 import Job from "../components/Home/Job";
 
 const Home = () => {
-
-  
   const [users, setUsers] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [usersPerPage, setUsersPerPage] = useState(10);
   const options = [
-    { label: 'New', value: 'New' },
-    { label: 'Old', value: 'Old' },
-    
+    { label: "New", value: "New" },
+    { label: "Old", value: "Old" },
   ];
   const handleSelectChange = (value) => {
-    console.log('Selected option value:', value);
+    console.log("Selected option value:", value);
   };
 
-  
- 
   useEffect(() => {
-    const firstTenUsers = usersData.slice(0, 10);
-        setUsers(firstTenUsers);
+    const firstTenUsers = usersData.slice(0, 100);
+    setUsers(firstTenUsers);
   }, []);
+
+  const indexOfLastUser = currentPage * usersPerPage;
+  const indexOfFirstUser = indexOfLastUser - usersPerPage;
+  const currentUsers = users.slice(indexOfFirstUser, indexOfLastUser);
+
+  const totalPages = Math.ceil(users.length / usersPerPage);
+
+  const handleClick = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const renderPagination = () => {
+    let pageNumbers = [];
+    for (let i = 1; i <= totalPages; i++) {
+      pageNumbers.push(
+        <li key={i} className={i === currentPage ? "active" : ""}>
+          <a onClick={() => handleClick(i)} href="#">
+            {i}
+          </a>
+        </li>
+      );
+    }
+    return <ul>{pageNumbers}</ul>;
+  };
 
   return (
     <div className="home container">
@@ -32,9 +53,12 @@ const Home = () => {
         <div className="row">
           <div className="col search">
             <h1>Search</h1>
-      
-            <Select options={options} defaultValue="Select" onChange={handleSelectChange}/>
 
+            <Select
+              options={options}
+              defaultValue="Select"
+              onChange={handleSelectChange}
+            />
           </div>
           <div className="col search-box">
             <form action="">
@@ -48,9 +72,12 @@ const Home = () => {
             <h4>20 jobs has been found</h4>
           </div>
         </div>
-        {users.map(user => (
-        <Job key={user.id} user={user}/>
+        {currentUsers.map((user) => (
+          <Job key={user.id} user={user} />
         ))}
+        <div className="row">
+        {renderPagination()}
+        </div>
       </div>
     </div>
   );
